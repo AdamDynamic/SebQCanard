d9a3bfa1fe4b43db981134766c9bbf701e0417ef
AdamDynamic <helloadamdynamic@gmail.com>
2014-12-12 20:48:15 +0000

    Modified user details

--- Automatron_ReferencePrivate.py
+++ Automatron_ReferencePrivate.py
@@ -17,13 +17,13 @@ DESCRIPTION:        Contains passwords and secret details used in the script
 # Define the database access details
 DB_HOST = "localhost"
 DB_USER = "root"
-DB_PASSWORD = "123penelope"
-DB_NAME = "Automatron"
+DB_PASSWORD = "password"
+DB_NAME = "database"
 
 DB_CONNECTTION_STRING = "mysql://root:123penelope@localhost/Automatron"
 
 # Define the keys used by the Twitter API to authenticate
-CONS_AUTOMATRON_SECRET = "mQPvfnTCbCFQcdS22RW9ChRQr90UM4p2lGrfz4ePikk"
-CONS_AUTOMATRON_TOKEN = "qZYq8TzCBVbXfnbMZzD3w"
-ACCESS_AUTOMATRON_SECRET = "HGNa5h8cN2q0q5FGSSaExE6D1cxbUHU76wIwVmS19JheP"
-ACCESS_AUTOMATRON_TOKEN = "2380500415-6bJiiWLe8YSrqeF3jnyl8AKI5tYPhRmxojo4m40"
+CONS_AUTOMATRON_SECRET = "InsertStringHere"
+CONS_AUTOMATRON_TOKEN = "InsertStringHere"
+ACCESS_AUTOMATRON_SECRET = "InsertStringHere"
+ACCESS_AUTOMATRON_TOKEN = "InsertStringHere"
